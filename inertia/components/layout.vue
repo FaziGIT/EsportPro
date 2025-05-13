865f7bd28a917aca710dc8c6272fe922e47d4ded
FaziGIT <mathis.tsrt@gmail.com>
2025-05-13 18:27:50 -0400

    feat(layout): add footer, sidebar and navbar components; and create layout to have the 3 components in one

---
+++ inertia/components/layout.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Navbar from './navbar.vue'
+import Sidebar from './sidebar.vue'
+import Footer from './footer.vue'
+</script>
+
+<template>
+  <div class="flex flex-col min-h-screen">
+    <Sidebar />
+
+    <div class="lg:ml-[114px] flex flex-col min-h-screen">
+      <Navbar />
+
+      <main class="flex-1 p-6">
+        <slot></slot>
+      </main>
+
+      <Footer />
+    </div>
+  </div>
+</template>
---
+++ inertia/pages/home/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import Layout from '~/components/layout.vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <Layout>
+    <p>{{ props.title }}</p>
+    <p>{{ props.description }}</p>
+  </Layout>
+</template>
