aedc3d6790bc3b280a3e7f017c03ece8a91f947c
Loeryx <altea95430@gmail.com>
2025-07-18 20:40:15 +0200

    PA-85 : Refacto

--- start/routes.ts
+++ start/routes.ts
@@ -23,6 +23,8 @@ const ProfileController = () => import('#controllers/profile_controller')
 
 const ErrorsController = () => import('#controllers/errors_controller')
 
+const SitemapController = () => import('#controllers/sitemap_controller')
+
 transmit.registerRoutes()
 
 // Public routes
@@ -90,7 +92,6 @@ router
     router.post('/profile/ban-user/:id/ban', [ProfileController, 'banUser'])
     router.post('/profile/unban-user/:id/unban', [ProfileController, 'unbanUser'])
     router.delete('/profile/delete-account', [ProfileController, 'deleteAccount'])
-    // router.get('/profile/user/:pseudo', [ProfileController, 'viewProfile'])
   })
   .use(middleware.auth())
 
@@ -99,95 +100,5 @@ router.get('/profile/user/:pseudo', [ProfileController, 'viewProfile'])
 // Error routes
 router.get('/unauthorized', [ErrorsController, 'unauthorized'])
 
-// Site maps dynamique pour les jeux et les tournois
-import Tournament from '#models/tournament'
-import Game from '#models/game'
-import { DateTime } from 'luxon'
-
-router.get('/sitemap.xml', async ({ response }) => {
-  const tournaments = await Tournament.all()
-  const games = await Game.all()
-
-  const tournamentUrls = tournaments.map(
-    (t) => `
-    <url>
-      <loc>https://esportpro.cloud/tournaments/${t.id}</loc>
-      <changefreq>weekly</changefreq>
-      <priority>0.6</priority>
-      <lastmod>${
-        t.updatedAt instanceof DateTime
-          ? t.updatedAt.toISODate()
-          : new Date().toISOString().split('T')[0]
-      }</lastmod>
-
-    </url>
-  `
-  )
-
-  const gameUrls = games.map(
-    (g) => `
-    <url>
-      <loc>https://esportpro.cloud/games/${g.id}</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.5</priority>
-      <lastmod>${
-        g.updatedAt instanceof DateTime
-          ? g.updatedAt.toISODate()
-          : new Date().toISOString().split('T')[0]
-      }</lastmod>
-
-    </url>
-  `
-  )
-
-  const staticUrls = [
-    {
-      loc: 'https://esportpro.cloud/',
-      changefreq: 'daily',
-      priority: '1.0',
-    },
-    {
-      loc: 'https://esportpro.cloud/search',
-      changefreq: 'weekly',
-      priority: '0.8',
-    },
-    {
-      loc: 'https://esportpro.cloud/tournaments',
-      changefreq: 'weekly',
-      priority: '0.8',
-    },
-    {
-      loc: 'https://esportpro.cloud/games',
-      changefreq: 'monthly',
-      priority: '0.7',
-    },
-    {
-      loc: 'https://esportpro.cloud/login',
-      changefreq: 'yearly',
-      priority: '0.3',
-    },
-    {
-      loc: 'https://esportpro.cloud/register',
-      changefreq: 'yearly',
-      priority: '0.3',
-    },
-  ]
-
-  const staticXml = staticUrls.map(
-    (u) => `
-    <url>
-      <loc>${u.loc}</loc>
-      <changefreq>${u.changefreq}</changefreq>
-      <priority>${u.priority}</priority>
-    </url>
-  `
-  )
-
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${[...staticXml, ...tournamentUrls, ...gameUrls].join('\n')}
-</urlset>`
-
-  response.header('Content-Type', 'application/xml')
-  return response.send(xml)
-})
+// Sitemap route
+router.get('/sitemap.xml', [SitemapController, 'generate'])
